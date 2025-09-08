e2fdb66f8d2d5de06afe27254cf1ae503c547e04
jiamudza <jiamudza@gmail.com>
2025-09-08 20:23:58 +0700

    initial setup

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashboardLayout = () => {
+  return (
+    <div className='flex'>
+    <div className='w-[14%] md:w-[16%] lg:w-[18%]'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default DashboardLayout
