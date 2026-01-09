edb68c74dbf2afa2d1232d913b60a34e0002a4dd
jiamudza <ajimasbagus12@gmail.com>
2026-01-09 21:39:43 +0700

    update eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["api.dicebear.com", "avatar.iran.liara.run", "res.cloudinary.com"]
+    domains: [
+      "api.dicebear.com",
+      "avatar.iran.liara.run",
+      "res.cloudinary.com",
+    ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 };
 
 export default nextConfig;
+
