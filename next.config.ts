42199ab02a33f07e0b961151d4cb47287b6bfe32
jiamudza <ajimasbagus12@gmail.com>
2026-06-20 03:18:42 +0700

    add new setting

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,30 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "api.dicebear.com",
-      "avatar.iran.liara.run",
-      "res.cloudinary.com",
-      "imgix2.ruangguru.com",
-    ],
+    // domains: [
+    //   "api.dicebear.com",
+    //   "avatar.iran.liara.run",
+    //   "res.cloudinary.com",
+    //   "imgix2.ruangguru.com",
+    // ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com'
+      },
+      {
+        protocol: 'https',
+        hostname: "avatar.iran.liara.run"
+      },
+      {
+        protocol: 'https',
+        hostname: "res.cloudinary.com"
+      },
+      {
+        protocol: 'https',
+        hostname: "imgix2.ruangguru.com"
+      },
+    ]
   },
 };
 
